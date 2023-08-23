5e82e7d9f0d4fb5aae8f3b90b75655091b526379
YOYO <r.h.fuentes.rodriguez@gmail.com>
2023-08-22 22:34:16 -0400

    actualizacion de portafolio

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,4 +1,5 @@
 import fotopf from "../PF.jpg"
+import solumetric from "../solumetric.jpg"
 
 export default function Projects() {
     return (
@@ -7,14 +8,25 @@ export default function Projects() {
                 Mis proyectos:
             </h3>
 
-            <div className='mt-5 flex justify-center gap-12 bg-grey py-5 border border-black'>
+            <div className='mt-5 flex flex-col lg:flex-row justify-center gap-12 bg-grey p-10 border border-black'>
                 <div className="hover:opacity-50 hover:cursor-pointer">
                     <a href="https://don-quijote.vercel.app/">
-                        <img src={fotopf} width="600"></img>
+                        <img src={fotopf} className="rounded-lg lg:w-screen"></img>
                         <h3 className="text-xl">Ecommerce de Libros</h3>
+                    </a>
+                </div>
+
+                <div className="hover:opacity-50 hover:cursor-pointer lg:w-screen">
+                    <a href="www.solumetric.cl">
+                        <img src={solumetric} className="rounded-lg "></img>
+                        <h3 className="text-xl">SoluMetric</h3>
                     </a>
                 </div>
             </div>
         </div>
     )
 }
+
+
+
+
